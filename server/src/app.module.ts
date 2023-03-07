ff85e452d109a4433594dd29d284fbc48aafd405
Ruslan <ruslan.mirets@gmail.com>
2023-03-07 12:40:39 +0300

    Register, Login, Access token [SERVER]

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { PrismaService } from "./prisma.service";
 import { AuthModule } from "./auth/auth.module";
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-	imports: [AuthModule],
+	imports: [ConfigModule.forRoot(), AuthModule],
 	controllers: [],
 	providers: [PrismaService],
 })
