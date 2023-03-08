b38063e4b8bbd5363308c08f9a72486d86f6fa92
Ruslan <ruslan.mirets@gmail.com>
2023-03-08 18:20:44 +0300

    Mail Module [SERVER]

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AuthModule } from "./auth/auth.module";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { UserModule } from './user/user.module';
+import { MailModule } from './mail/mail.module';
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule, UserModule],
+	imports: [ConfigModule.forRoot(), AuthModule, UserModule, MailModule],
 	controllers: [],
 	providers: [PrismaService],
 })
