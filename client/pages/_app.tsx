5c101602f416f9488ff3e1573ea527199c09498a
Ruslan <ruslan.mirets@gmail.com>
2023-03-08 15:22:08 +0300

    Store, redux-toolkit [CLIENT]

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,26 @@
 import "@/assets/styles/globals.scss";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { persistor, store } from "@/store/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+		},
+	},
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Provider store={store}>
+				<PersistGate loading={null} persistor={persistor}>
+					<Component {...pageProps} />
+				</PersistGate>
+			</Provider>
+		</QueryClientProvider>
+	);
 }
---
+++ client/src/hooks/useAuth.ts
@@ -0,0 +1,6 @@
+import { useTypedSelector } from "./useTypedSelector";
+
+export const useAuth = () =>
+	useTypedSelector((state) => {
+		state.user;
+	});
